7d75f5f171cc05a71b77fcb6d6bccb704dd5ebec
rafinha20010 <rafaelsanchesperacine@gmail.com>
2025-05-26 15:09:02 -0300

    a

---
+++ pages/dashboard/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const userToken = JSON.parse(localStorage.getItem('token')) || {};
+    const userData = JSON.parse(localStorage.getItem('user')) || {};
+    
+    if (userToken.firstName) {
+        document.getgokuElementById('user-name').textContent = `Olá, ${userData.firstName}!`;
+        document.getElementById('user-email').textContent = userData.email;
+    } else {
+        // Redirecionar para a página de login se não houver usuário
+        window.location.href = '../login/login.html';
+    }
+    
+    // Botão de logout
+    document.getElementById('logout-btn').addEventListener('click', function() {
+        localStorage.removeItem('token');
+        window.location.href = '/';
+    });
+    
+    // Toggle do menu lateral (para mobile)
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.toggle('active');
+    });
+    
+    // Fechar menu ao clicar em um item (para mobile)
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            if (window.innerWidth <= 992) {
+                document.getElementById('sidebar').classList.remove('active');
+            }
+        });
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    imageMapResize();
+});
+
+<script src="https://cdn.jsdelivr.net/npm/image-map-resizer@1.0.10/js/imageMapResizer.min.js"></script>
--- pages/login/script.js
+++ pages/login/script.js
@@ -19,7 +19,8 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
         
         if (response.ok) {
             // Salvar dados do usuário no localStorage (para demonstração)
-            localStorage.setItem('user', JSON.stringify(data.token)); // 2
+            localStorage.setItem('token', JSON.stringify(data.token)); // 2
+            localStorage.setItem('user', JSON.stringify(data.user));
             
             // Redirecionar para a página principal (após login bem-sucedido)
             alert('Login realizado com sucesso!');
