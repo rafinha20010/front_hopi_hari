6187d60b698c0afd0a2822147690e6cf8e3daf85
rafinha20010 <rafaelsanchesperacine@gmail.com>
2025-05-27 11:12:50 -0300

    lk

--- areas/script.js
+++ areas/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const area = "Mistieri";
     const token = localStorage.getItem('token');
-    const response = await fetch(`http://localhost:3000/brinquedos/${area}`, {
+    const response = await fetch(`http://localhost:3000/brinquedos/area/${area}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
