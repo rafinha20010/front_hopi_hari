e9be3f09b6291db93015846bc72b1036f2a4f703
rafinha20010 <rafaelsanchesperacine@gmail.com>
2025-06-10 11:10:36 -0300

    area

--- areas/script.js
+++ areas/script.js
@@ -15,19 +15,20 @@ document.addEventListener('DOMContentLoaded', async function() {
     const data = await response.json();
     console.log(data);
     
-    for(var brinquedos of data){
+    for(var brinquedos of data.resultados){
         console.log(brinquedos);
     }
 
 
-});
-const grid = document.querySelector('.grid');
+
+const grid = document.querySelector('.rides-grid');
 grid.innerHTML = grid.innerHTML + 
 
                    ` <div class="ride-card">
-                <div class="ride-image" style="background-image: url('https://36b7b97122.cbaul-cdnwnd.com/a929c8f5bc1fba6c13d5199567947203/system_preview_detail_200000075-c5003c579f/Kastel%20di%20Lendas.png')"></div>
+                <div class="ride-image" style="background-image: url('../../images/${brinquedos.image}')"></div>
                 <div class="ride-info">
                     <h3 class="ride-name">${brinquedo.name}</h3>
                     <div class="ride-time">${brinquedo.waiting_time}</div>
                     <span class="ride-status status-open">${brinquedo.status}</span>
-                </div>`;
+                </div>`
+})
